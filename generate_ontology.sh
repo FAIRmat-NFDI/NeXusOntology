373a135a545ada31b5bbabe7c1f11bf51ba1c182
Tanmay2028 <tanmay.kulkarni@fau.de>
2025-05-25 22:16:49 +0200

    Add nw commit hash in generate_ontology.sh

--- generate_ontology.sh
+++ generate_ontology.sh
@@ -1,3 +1,3 @@
 # This script generates the NeXus ontology in OWL format.
 python -m script.generate_ontology
-python -m script.generate_ontology full 71be6c2eeb2415afea598b6c21c5f402ffe18d00
+python -m script.generate_ontology full 60eb3449d8aa1025a42ee652c8b85b0b684931ec
