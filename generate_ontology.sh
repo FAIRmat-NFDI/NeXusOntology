68411d6172de24669431643ac57dc87696097a51
Tanmay2028 <tanmay.kulkarni@fau.de>
2025-07-30 09:16:23 +0200

    Update definitions submodule and ontology

--- generate_ontology.sh
+++ generate_ontology.sh
@@ -1,3 +1,3 @@
 # This script generates the NeXus ontology in OWL format.
 python -m script.generate_ontology
-python -m script.generate_ontology full 60eb3449d8aa1025a42ee652c8b85b0b684931ec
+python -m script.generate_ontology full 023c5f0889cce9e37c8028e941e04ebba126273e
